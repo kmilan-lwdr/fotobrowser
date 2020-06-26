634da6015d2f69b5c6370b442d8ea4e3743ac738
Kim Milán <milan.knp@gmail.com>
2020-06-26 10:14:50 +0300

    Updates

---
+++ precache-manifest.7af4524c8694912caba37cb065dc5355.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bcba49ab3d643d8d1e0b46136ca725",
+    "url": "/fotobrowser/index.html"
+  },
+  {
+    "revision": "2c5e1cba19c64b636959",
+    "url": "/fotobrowser/static/css/main.538639c0.chunk.css"
+  },
+  {
+    "revision": "04e24bcc43a18d13888c",
+    "url": "/fotobrowser/static/js/2.8f99dc8f.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/fotobrowser/static/js/2.8f99dc8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5e1cba19c64b636959",
+    "url": "/fotobrowser/static/js/main.fe95659f.chunk.js"
+  },
+  {
+    "revision": "72b867639b4bb2542766",
+    "url": "/fotobrowser/static/js/runtime-main.bb87e41c.js"
+  },
+  {
+    "revision": "da010917cb085a33fe78c79b5c94d970",
+    "url": "/fotobrowser/static/media/bg.da010917.svg"
+  },
+  {
+    "revision": "d768250149eb4edfb9d3104bb4e88437",
+    "url": "/fotobrowser/static/media/frown.d7682501.svg"
+  },
+  {
+    "revision": "fda1130cfac03cc431faa52a964fdfba",
+    "url": "/fotobrowser/static/media/loader.fda1130c.svg"
+  }
+]);
